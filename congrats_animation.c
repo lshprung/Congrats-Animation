ff323cdde3148a51321685e353a40a270f7795ea
loshprung <lshprung@scu.edu>
2020-02-27 18:59:22 -0800

    added basic color changing

--- congrats_animation.c
+++ congrats_animation.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #define DISTANCE 10
 #define DOWNTIME 100000
 
 int main(){
 	int i, j;
+	int num_rand; //for random color
+	int counter = 1; //change color when hits 5
+
+	srand(time(NULL));
 	
 	while(1){
 		for(i = 0; i < DISTANCE; i++){
+			counter++;
+
 			for(j = 0; j < 15+i+1; j++){
 				printf("\b \b"); //backspace
 			}
 			for(j = 0; j < i; j++){
 				printf(" ");
+			}
+			if(counter == 5){
+				num_rand = (rand() % 7) + 1;
+				printf("\e[0;3%dm", num_rand);
+				counter = 1;
 			}
 			printf("Congratulations");
 			fflush(stdout);
